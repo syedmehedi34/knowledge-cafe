1a44ce4e802e11cb7173d2aa75dd0a570756be1c
syedmehedi34 <syedmeehdi34@gmail.com>
2024-10-21 23:56:49 +0600

    nav portion done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div>
+    <div className="w-11/12 mx-auto">
       <Header></Header>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import profilePic from "../../assets/image/profile.jpg";
+//
+
 const Header = () => {
   return (
-    <div>
+    <div className="mb-8 flex justify-between items-center mt-8 pb-7 border-b border-[#11111126">
       <h1 className="text-4xl">Knowledge Cafe</h1>
+      <div className="w-14 h-14 rounded-full">
+        <img
+          className="w-14 h-14 rounded-full object-cover"
+          src={profilePic}
+          alt=""
+        />
+      </div>
     </div>
   );
 };
